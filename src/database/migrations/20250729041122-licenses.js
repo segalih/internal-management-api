a12947ce805eb4df4628ed77a9b71112e1e6217a
Galih S <galihsetyawan.work@gmail.com>
2025-07-29 11:15:38 +0700

    feat: create licenses migration with necessary fields and timestamps

---
+++ src/database/migrations/20250729041122-licenses.js
@@ -0,0 +1,69 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+
+    await queryInterface.createTable('licenses', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      pks: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      bast: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      aplikasi: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      due_date_license: {
+        type: Sequelize.DATE,
+        allowNull: true,
+      },
+      health_check_routine: {
+        type: Sequelize.DATE,
+        allowNull: true,
+      },
+      health_check_actual: {
+        type: Sequelize.DATE,
+        allowNull: true,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'),
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'),
+      },
+      deleted_at: {
+        type: Sequelize.DATE,
+        defaultValue: null,
+        allowNull: true,
+      },
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+
+    await queryInterface.dropTable('licenses');
+  },
+};
