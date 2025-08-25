1b7aa66eb8472cee0cec74887a3f0df51b2c96a4
Galih Setyawan <galih.kamvax032@gmail.com>
2025-08-25 14:38:40 +0700

    refactor ♻️: update seeders with new application names
    Updated the database seeder to include new application names, removing outdated ones and adding new ones as per the latest requirements. This change ensures that the database reflects the current state of applications.

--- src/database/seeders/20250823071713-pic.js
+++ src/database/seeders/20250823071713-pic.js
@@ -16,7 +16,7 @@ module.exports = {
     await queryInterface.bulkInsert('person_in_charge_master', [
       { person_name: 'Cahyo Adhi Hartanto', flag: 1 },
       { person_name: 'Achmad Zain Baichuni', flag: 1 },
-      { person_name: 'Rivaldo', flag: 1 },
+      { person_name: 'Rivaldo Erdany Putra', flag: 1 },
       { person_name: 'Shandy Zolla Pratama', flag: 1 },
       { person_name: 'Nadya Siti Nurbaeti', flag: 1 },
       { person_name: 'Deris Ganesha Pratama', flag: 1 },
@@ -24,10 +24,10 @@ module.exports = {
       { person_name: 'Rizky Dwi Ismantara', flag: 1 },
       { person_name: 'Fatan Akbar Yogenta', flag: 1 },
       { person_name: 'Dandy Fajar Pratama', flag: 1 },
-      { person_name: 'Unggul', flag: 1 },
-      { person_name: 'Leo', flag: 1 },
-      { person_name: 'Dimas Widhi', flag: 1 },
-      { person_name: 'Dhimas Wicak', flag: 1 },
+      { person_name: 'Unggul Prayuda', flag: 1 },
+      { person_name: 'Leonardo Robinsar Agustinus Sianturi', flag: 1 },
+      { person_name: 'Dimas Widhi Prihatmadja', flag: 1 },
+      { person_name: 'Dhimas Wicaksana Andrian', flag: 1 },
     ]);
   },
 
