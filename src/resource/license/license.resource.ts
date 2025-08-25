ebd3003c672cf2fbafb9d2148e97642bf6549282
Galih Setyawan <galih.kamvax032@gmail.com>
2025-08-25 10:00:45 +0700

    feat ✨: add due date color status to license resource
    Updated the license resource to include a color status for due dates, with green indicating less than 30 days, yellow indicating between 30-90 days, and red indicating more than 90 days.

--- src/resource/license/license.resource.ts
+++ src/resource/license/license.resource.ts
@@ -1,7 +1,16 @@
 import License, { LicenseAttributes } from '@database/models/license.model';
 import { healthchecksResource } from './healthchecks.resource';
+import { DateTime } from 'luxon';
 
 export const licenseResource = (data: License): LicenseAttributes => {
+  const dueDate = DateTime.fromISO(data.dueDateLicense.toString(), { zone: 'UTC' });
+  const dayTodaytoDueDate = dueDate.diffNow('days').days;
+  let colorStatus = 'green';
+  if (dayTodaytoDueDate < 30) {
+    colorStatus = 'red';
+  } else if (dayTodaytoDueDate < 90) {
+    colorStatus = 'yellow';
+  }
   return {
     id: data.id,
     pks: data.pks,
@@ -10,6 +19,7 @@ export const licenseResource = (data: License): LicenseAttributes => {
     filePks: data.filePks,
     fileBast: data.fileBast,
     isNotified: data.isNotified,
+    status: colorStatus,
     healthchecks: data.healthchecks && data.healthchecks.map((healthcheck) => healthchecksResource(healthcheck)),
   };
 };
