959cf6aa1ec0f34edece5306ce15471d8b35d966
Galih Setyawan <galih.kamvax032@gmail.com>
2025-08-28 10:54:52 +0700

    refactor ♻️: improved validation middleware by adding a flattenErrors function to handle nested error objects
    The changes improve the validation middleware by introducing a new function, `flattenErrors`, which recursively flattens nested error objects. This enhancement enhances the overall robustness and readability of the codebase.

--- src/middleware/validation.middleware.ts
+++ src/middleware/validation.middleware.ts
@@ -5,21 +5,43 @@ import { HttpStatusCode } from 'axios';
 import { ResponseApi } from '@helper/interface/response.interface';
 import { messages } from '@config/message';
 
+function flattenErrors(errors: ValidationError[], parent?: string): any[] {
+  return errors.flatMap((error) => {
+    const propertyPath = parent ? `${parent}.${error.property}` : error.property;
+
+    if (error.constraints) {
+      return [
+        {
+          property: propertyPath,
+          constraints: error.constraints,
+        },
+      ];
+    }
+
+    if (error.children && error.children.length > 0) {
+      return flattenErrors(error.children, propertyPath);
+    }
+
+    return [];
+  });
+}
 
 export function validationMiddleware<T extends object>(type: new () => T) {
   return async (req: Request, res: Response, next: NextFunction) => {
     const dto = plainToInstance(type, req.body);
-    const errors: ValidationError[] = await validate(dto);
+    const errors: ValidationError[] = await validate(dto, {
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    });
 
     if (errors.length > 0) {
+      const flatErrors = flattenErrors(errors);
+
       const errorResponse: ResponseApi<any> = {
         statusCode: HttpStatusCode.BadRequest,
         message: messages.VALIDATION_ERROR,
         data: {},
-        errors: errors.map((error) => ({
-          property: error.property,
-          constraints: error.constraints,
-        })),
+        errors: flatErrors,
       };
 
       if (req.file) {
@@ -30,9 +52,7 @@ export function validationMiddleware<T extends object>(type: new () => T) {
         }
       }
 
-      return res.status(HttpStatusCode.BadRequest).json({
-        ...errorResponse,
-      });
+      return res.status(HttpStatusCode.BadRequest).json(errorResponse);
     }
 
     next();
