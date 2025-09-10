3debfd1cd4cee9c52530b40f7204fed28058f562
Galih Setyawan <galih.kamvax032@gmail.com>
2025-09-10 16:54:44 +0700

    feat ✨: added seed data for master groups and departments
    Created new seeder files for master_groups and master_departments tables to populate the database with initial data. This will provide a starting point for the application, allowing users to test and validate the functionality before proceeding.

---
+++ src/database/seeders/20250910095106-departments.js
@@ -0,0 +1,31 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+     */
+    await queryInterface.bulkInsert('master_departments', [
+      { name: 'Financing & Collection Services' },
+      { name: 'Release' },
+      { name: 'Core' },
+    ]);
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    await queryInterface.bulkDelete('master_departments', null, {});
+  },
+};
---
+++ src/database/seeders/20250910095109-vendors.js
@@ -0,0 +1,27 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+     */
+    await queryInterface.bulkInsert('master_vendors', [{ name: 'ADIDATA' }, { name: 'MITECH' }, { name: 'AIgen' }]);
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    await queryInterface.bulkDelete('master_vendors', null, {});
+  },
+};
