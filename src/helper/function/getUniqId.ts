c2f6fd9bf4afc3afac1be2cb4fa66417378007c0
Galih Setyawan <galih.kamvax032@gmail.com>
2025-09-11 22:16:41 +0700

    revert ⏪: revert removal of unused function
    Removed the unused `getUniqId` function from the codebase. This change was made to simplify the code and remove unnecessary dependencies.

--- src/helper/function/getUniqId.ts
+++
@@ -1,7 +0,0 @@
-import generateUniqueId from 'generate-unique-id';
-interface IGetUniqId {
-  length?: number;
-}
-export function getUniqId(input: IGetUniqId = { length: 8 }): string {
-  return generateUniqueId(input).toLowerCase();
-}
